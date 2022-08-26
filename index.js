a4e88574c77f5d1711b4c72b95cd08f17737c62b
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-08-25 22:53:08 -0300

    mongoDb implementado

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/characters.route');
+const connectMongoDB = require('./src/database/database');
 
 const port = 3000;
 const app = express();
+
+connectMongoDB();
 
 app.use(express.json());
 app.use(cors());
