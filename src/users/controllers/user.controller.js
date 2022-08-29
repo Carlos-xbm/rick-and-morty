cf22c5fda276ec1ff49c64b16b909b14272f58dd
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-08-29 20:27:50 -0300

    iniciado

---
+++ src/users/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const createUserController = async (req, res) => {
+  res.send({ message: 'Create ok' });
+};
+
+const findAllUserController = async (req, res) => {
+  res.send({ message: 'Find all ok' });
+};
+
+module.exports = { createUserController, findAllUserController };
