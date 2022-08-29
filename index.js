5c86240c096a5572bfc6389593e65af4249aa573
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-08-29 20:01:13 -0300

    editados

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/characters.route');
 const connectMongoDB = require('./src/database/database');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 connectMongoDB();
 
 app.use(express.json());
 app.use(cors());
+
+/* SWAGGER */
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use('/rick-and-morty', routes);
 
