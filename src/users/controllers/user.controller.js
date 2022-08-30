9ea222515d62a2fc09f3308cc56d649c2d2f6e67
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-08-29 21:22:42 -0300

    find all implementado

--- src/users/controllers/user.controller.js
+++ src/users/controllers/user.controller.js
@@ -21,7 +21,12 @@ const createUserController = async (req, res) => {
 };
 
 const findAllUserController = async (req, res) => {
-  res.send({ message: 'Find all ok' });
+  const users = await userService.findAllUserService();
+
+  if (users.length === 0) {
+    return res.status(400).send({ message: 'Não existe Usuários cadastrado' });
+  }
+  res.send(users);
 };
 
 module.exports = { createUserController, findAllUserController };
--- src/users/services/user.service.js
+++ src/users/services/user.service.js
@@ -2,5 +2,6 @@ const User = require('../models/user');
 
 const findByEmailUserService = (email) => User.findOne({ email: email });
 const createUserService = (body) => User.create(body);
+const findAllUserService = () => User.find();
 
-module.exports = { findByEmailUserService, createUserService };
+module.exports = { findByEmailUserService, createUserService, findAllUserService };
