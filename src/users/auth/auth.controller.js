4d81ebddb231c431b9600b657ecf64e95d8945d2
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-08-29 23:11:07 -0300

    login controller e service implementado

--- src/users/auth/auth.controller.js
+++ src/users/auth/auth.controller.js
@@ -1,7 +1,19 @@
 const authService = require('./auth.service');
+const bcrypt = require('bcryptjs');
 
 const loginController = async (req, res) => {
-  res.send({ message: 'Login OK' });
+  const { email, password } = req.body;
+  const user = await authService.loginService(email);
+
+  if (!user) {
+    return res.status(400).send({ message: 'Usuario não encontrado' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).send({ message: 'senha invalida' });
+  }
+  res.send(user);
 };
 
 module.exports = { loginController };
